d59560f30a36341f572def30ea5c92621905bbd3
Tom Nuen <tomnuen@agiper.com>
2019-07-26 21:57:15 +0700

    added CDN CSS

--- Iconlook.Service.Web/Pages/WeatherForecastService.cs
+++ Iconlook.Service.Web/Pages/WeatherForecastService.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Iconlook.Entity;
 
-namespace Iconlook.Service.Web.Data
+namespace Iconlook.Service.Web.Pages
 {
     public class WeatherForecastService
     {
--- Iconlook.Service.Web/Startup.cs
+++ Iconlook.Service.Web/Startup.cs
@@ -1,4 +1,4 @@
-using Iconlook.Service.Web.Data;
+using Iconlook.Service.Web.Pages;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
