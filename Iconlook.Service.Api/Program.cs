a4308e5aee1551b81072ef615d9b089013fe38d0
Tom Nuen <tomnuen@agiper.com>
2019-07-26 21:26:28 +0700

    added API service

---
+++ Iconlook.Service.Api/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Iconlook.Service.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
