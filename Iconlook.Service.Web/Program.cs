9a61b6e373187a1067dbab81c0e126a9a7e09bd1
Tom Nuen <tomnuen@agiper.com>
2019-07-28 00:45:41 +0700

    changed

--- Iconlook.Service.Web/Program.cs
+++ Iconlook.Service.Web/Program.cs
@@ -1,13 +1,47 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Hangfire;
+using Iconlook.Service.Web.Pages;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceStack;
+using Syncfusion.Licensing;
 
 namespace Iconlook.Service.Web
 {
-    public class Program
+    public class Program : Agiper.Server.Program
     {
-        public static void Main()
+        public static async Task Main()
         {
-            Host.CreateDefaultBuilder().ConfigureWebHostDefaults(x => x.UseUrls("http://*:80/").UseStartup<Startup>()).Build().Run();
+            ConfigureServices = host => services =>
+            {
+                services.AddRazorPages();
+                services.AddHangfire(x => {});
+                services.AddResponseCaching();
+                services.AddServerSideBlazor();
+                services.AddResponseCompression();
+                services.AddSingleton<WeatherForecastService>();
+                services.AddDataProtection()
+                    .PersistKeysToFileSystem(new DirectoryInfo("/var/lib/dotnet"))
+                    .SetApplicationName(Assembly.GetEntryAssembly().GetName().Name);
+            };
+            Configure = host => application =>
+            {
+                application.UseResponseCaching();
+                application.UseResponseCompression();
+                application.UseStaticFiles();
+                application.UseRouting();
+                application.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapBlazorHub();
+                    endpoints.MapFallbackToPage("/_host");
+                });
+                application.UseServiceStack(host);
+                SyncfusionLicenseProvider.RegisterLicense("MTI1Mjk1QDMxMzcyZTMyMmUzMEhsKzRudFN1U00rd2");
+            };
+            await StartAsync(new WebHost(), "http://*:80/");
         }
     }
 }
