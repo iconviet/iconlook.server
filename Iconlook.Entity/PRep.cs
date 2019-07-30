729c3ff3059c304b158cdcbf0af08d2989329d13
Tom Nuen <tomnuen@iconviet.io>
2019-07-30 14:04:33 +0700

    test demo again

---
+++ Iconlook.Entity/PRep.cs
@@ -0,0 +1,8 @@
+﻿namespace Iconlook.Entity
+{
+    public class PRep
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ Iconlook.Entity/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace Iconlook.Entity
+{
+    public class Transaction
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public decimal Fee { get; set; }
+        public string Hash { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
