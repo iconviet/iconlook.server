a34672c76586c8a9874532c8ded4a7fca950c1ee
Tom Nuen <tomnuen@iconviet.io>
2019-07-31 18:15:07 +0700

    nice

--- Iconlook.Entity/PRep.cs
+++ Iconlook.Entity/PRep.cs
@@ -2,7 +2,12 @@
 {
     public class PRep
     {
+        public int Rank { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
+        public string LogoUrl { get; set; }
+        public string Created { get; set; }
+        public string Address { get; set; }
         public string Location { get; set; }
     }
 }
--- Iconlook.Service.Api/PrepService.cs
+++ Iconlook.Service.Api/PrepService.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Agiper;
 using Agiper.Server;
+using HtmlAgilityPack;
 using Iconlook.Entity;
+using ServiceStack;
 
 namespace Iconlook.Service.Api
 {
@@ -16,11 +18,20 @@ namespace Iconlook.Service.Api
 
         public async Task<List<PRep>> GetLatestPRepsAsync()
         {
-            return Enumerable.Range(1, 12).Select(x => new PRep
-            {
-                Name = new[] { "ALFKI", "ANANTR", "ANTON", "BLONP", "BOLID" }[new Random().Next(5)],
-                Location = new[] { "ALFKI", "ANANTR", "ANTON", "BLONP", "BOLID" }[new Random().Next(5)]
-            }).ToList();
+            var rank = new Stack<int>(Enumerable.Range(1, 60));
+            var html = await new HtmlWeb().LoadFromWebAsync("https://icon.community/iconsensus/candidates");
+            var data = (from t in html.DocumentNode.SelectNodes("//tbody")
+                        from r in t.SelectNodes("tr")
+                        select new PRep
+                        {
+                            Rank = rank.Pop(),
+                            Created = r.SelectNodes("td")[1].InnerText.Trim(),
+                            Name = r.SelectNodes("td")[2].InnerText.Trim().ToTitleCase(),
+                            Id = r.SelectSingleNode("td/a").GetAttributeValue("href", "0").Split('/').ElementAt(3),
+                            LogoUrl = $"https://icon.community{r.SelectSingleNode("td/div/img").GetAttributeValue("src", null)}",
+                            Location = r.SelectNodes("td")[3].InnerText.Trim().Split(',').LastOrDefault().ToLower().ToTitleCase()
+                        }).OrderBy(x => x.Rank).Shuffle().Take(12).ToList();
+            return data;
         }
     }
 }
