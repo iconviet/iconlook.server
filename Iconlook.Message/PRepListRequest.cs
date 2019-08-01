decfd6a41f0d41e75b1ed366cdba46c2c6d64b22
Tom Nuen <tomnuen@iconviet.io>
2019-08-01 17:13:19 +0700

    c

--- Iconlook.Message/PRepListRequest.cs
+++ Iconlook.Message/PRepListRequest.cs
@@ -3,7 +3,7 @@ using ServiceStack;
 
 namespace Iconlook.Message
 {
-    [Route("preps", "GET")]
+    [Route("/preps", "GET")]
     public class PRepListRequest : ListRequestBase<PRepListRequest, PRepResponse>, IGet
     {
     }
