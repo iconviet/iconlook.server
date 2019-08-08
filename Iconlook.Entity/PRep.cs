ca203664f3fad7f0d914f66791bf2d2a1d667148
Tom Nuen <tomnuen@iconviet.io>
2019-08-09 00:15:07 +0700

    c

--- Iconlook.Entity/PRep.cs
+++ Iconlook.Entity/PRep.cs
@@ -3,9 +3,9 @@
     public class PRep
     {
         public int Votes { get; set; }
+        public int Score { get; set; }
         public string Id { get; set; }
         public int Voters { get; set; }
-        public int CScore { get; set; }
         public string Name { get; set; }
         public int Position { get; set; }
         public bool Direction { get; set; }
