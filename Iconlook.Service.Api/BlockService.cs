61967b4fb7d7eff6808531583885463b1aa22b4e
Tom Nuen <tomnuen@iconviet.io>
2019-08-13 21:16:13 +0700

    added tabs

--- Iconlook.Service.Api/BlockService.cs
+++ Iconlook.Service.Api/BlockService.cs
@@ -12,14 +12,14 @@ namespace Iconlook.Service.Api
         [CacheResponse(Duration = 60, MaxAge = 30)]
         public object Any(BlockListRequest request)
         {
-            return new ListResponse<BlockResponse>(Enumerable.Range(1, 12).Select(x => new BlockResponse
+            return new ListResponse<BlockResponse>(Enumerable.Range(1, 20).Select(x => new BlockResponse
             {
                 To = new[] { "hx522b...2e84", "hx522b...2e84", "hx522b...2e84", "hx522b...2e84", "hx522b...2e84" }[new Random().Next(5)],
                 From = new[] { "hx522b...2e84", "hx522b...2e84", "hx522b...2e84", "hx522b...2e84", "hx522b...2e84" }[new Random().Next(5)],
                 Hash = new[] { "0x07c3....bc543", "0x07c3....bc543", "0x07c3....bc543", "0x07c3....bc543", "0x07c3....bc543" }[new Random().Next(5)],
                 Amount = new decimal[] { 100, 200, 300, 400, 500 }[new Random().Next(5)],
                 Fee = new decimal[] { 1, 2, 3, 4, 5 }[new Random().Next(5)]
-            }).ToList());
+            }).Take(request.Take).ToList());
         }
     }
 }
