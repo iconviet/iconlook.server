f738c2776be3e605e68cbd5f8cc250be63644c8e
Tom Nuen <tomnuen@agiper.com>
2019-08-17 21:25:00 +0700

    c

---
+++ Iconlook.Service.Web/Jobs/BlockProductionJob.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Agiper.Server;
+
+namespace Iconlook.Service.Web.Jobs
+{
+    public class BlockProductionJob : JobBase
+    {
+        public void Run()
+        {
+            Console.WriteLine("Block Production Monitor Job Ran.");
+        }
+    }
+}
