008577c82b541ab54c6d8687dc793d51af1b1712
Tom Nuen <tomnuen@iconviet.io>
2019-09-25 16:03:10 +0700

    c

---
+++ Iconlook.Service.Web/Sources/BlockSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DynamicData;
+using Iconlook.Object;
+
+namespace Iconlook.Service.Web.Sources
+{
+    public class BlockSource : SourceCache<BlockResponse, int>
+    {
+        public BlockSource(Func<BlockResponse, int> selector) : base(selector)
+        {
+        }
+    }
+}
---
+++ Iconlook.Service.Web/Sources/Source.cs
@@ -0,0 +1,12 @@
+﻿namespace Iconlook.Service.Web.Sources
+{
+    public static class Source
+    {
+        public static BlockSource Blocks { get; }
+
+        static Source()
+        {
+            Blocks = new BlockSource(x => x.Height);
+        }
+    }
+}
