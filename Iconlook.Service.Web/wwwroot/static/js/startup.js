8704081de4b7774a4f412bf06074c069dcfafd3f
Tom Nuen <tomnuen@iconviet.io>
2019-10-10 10:51:49 +0700

    Revert "changed"
    This reverts commit 194cc7e104c99b9cb635dc1ec4f8cffbc27146f7.

--- Iconlook.Service.Web/wwwroot/static/js/startup.js
+++ Iconlook.Service.Web/wwwroot/static/js/startup.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function() {
     console.log("Ready!!!!!");
-    const source = new EventSource('/sse/stream?channel=iconlook');
+    var source = new EventSource('/sse/stream?channel=iconlook');
     source.addEventListener('error', function (e) {
         console.log("ERROR", e);
     }, false);
@@ -12,19 +12,11 @@
         },
         success: function (selector, json, message) {
             if (!selector.startsWith('cmd.on')) {
-                const block_grid = document.getElementById('block_grid');
-                if (block_grid != null) {
-                    if (message.cmd === 'BlockProducedSignal') {
-                        const instance = block_grid.ej2_instances[0];
-                        // instance.addRecord(json.block);
-                        console.log("Received " + message.cmd, json);
-                    }
-                }
-
+                // console.log("Received " + message.cmd, json);
             }
         }
     });
-    const click = window.rxjs.fromEvent(document, 'click');
-    const example = click.pipe(window.rxjs.operators.map(event => 'Event time: ${event.timeStamp}'));
+    const click = rxjs.fromEvent(document, 'click');
+    const example = click.pipe(rxjs.operators.map(event => `Event time: ${event.timeStamp}`));
     example.subscribe(val => console.log(val));
 })
