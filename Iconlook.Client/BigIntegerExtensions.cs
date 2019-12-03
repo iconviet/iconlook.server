e6bd1abe4d4557b44fd9ddb66585ff56597666f7
Tom Nuen <tomnuen@iconviet.io>
2019-12-03 10:50:41 +0700

    c

--- Iconlook.Client/BigIntegerExtensions.cs
+++ Iconlook.Client/BigIntegerExtensions.cs
@@ -1,9 +1,14 @@
 ﻿using System.Numerics;
 
-namespace Iconlook.Service.Job
+namespace Iconlook.Client
 {
     public static class BigIntegerExtensions
     {
+        public static decimal ToDecimal(this BigInteger instance)
+        {
+            return decimal.Parse(BigInteger.Divide(instance, BigInteger.Pow(10, 18)).ToString());
+        }
+
         public static BigInteger DividePow(this BigInteger instance, BigInteger value, int exponent)
         {
             return BigInteger.Divide(instance, BigInteger.Pow(value, exponent));
--- Iconlook.Client/Data/PRep.cs
+++ Iconlook.Client/Data/PRep.cs
@@ -1,4 +1,6 @@
-﻿using Lykke.Icon.Sdk.Transport.JsonRpc;
+﻿using System.Numerics;
+using Lykke.Icon.Sdk.Data;
+using Lykke.Icon.Sdk.Transport.JsonRpc;
 
 namespace Iconlook.Client.Data
 {
@@ -14,6 +16,46 @@ namespace Iconlook.Client.Data
         public string GetName()
         {
             return _properties.GetItem("name")?.ToString();
+        }
+
+        public string GetCity()
+        {
+            return _properties.GetItem("city")?.ToString();
+        }
+
+        public string GetCountry()
+        {
+            return _properties.GetItem("country")?.ToString();
+        }
+
+        public Address GetAddress()
+        {
+            return _properties.GetItem("address")?.ToAddress();
+        }
+
+        public BigInteger GetIrep()
+        {
+            return _properties.GetItem("irep")?.ToInteger() ?? 0;
+        }
+
+        public BigInteger GetStake()
+        {
+            return _properties.GetItem("stake")?.ToInteger() ?? 0;
+        }
+
+        public BigInteger GetDelegated()
+        {
+            return _properties.GetItem("delegated")?.ToInteger() ?? 0;
+        }
+
+        public BigInteger GetTotalBlocks()
+        {
+            return _properties.GetItem("totalBlocks")?.ToInteger() ?? 0;
+        }
+
+        public BigInteger ValidatedBlocks()
+        {
+            return _properties.GetItem("validatedBlocks")?.ToInteger() ?? 0;
         }
     }
 }
