0cb59d6c0e8e09997bfd0fef26bcb3ac38383c60
Tom Nuen <tomnuen@iconviet.io>
2019-12-23 14:36:38 +0700

    changed

--- Iconlook.Service.Web/Pages/Index.razor.cs
+++ Iconlook.Service.Web/Pages/Index.razor.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Iconlook.Object;
+using Iconlook.Server;
+using ServiceStack.Redis;
 using Syncfusion.EJ2.Blazor.Navigations;
 using Syncfusion.EJ2.Blazor.SplitButtons;
 
@@ -42,11 +45,11 @@ namespace Iconlook.Service.Web.Pages
             Production = new TabHeader { Text = "PRODUCTION", IconCss = "fal fa-server" };
             Transactions = new TabHeader { Text = "TRANSACTIONS", IconCss = "fal fa-repeat" };
             Governance = new TabHeader { Text = "GOVERNANCE", IconCss = "fal fa-users-class" };
-            // using (var redis = Host.Current.Resolve<IRedisClient>())
-            // {
-            //     PeerResponse = redis.As<PeerResponse>().GetAll().FirstOrDefault(x => x.State == "BlockGenerate");
-            //     ChainResponse = redis.As<ChainResponse>().GetAll().OrderByDescending(x => x.Timestamp).FirstOrDefault();
-            // }
+            using (var redis = Host.Current.Resolve<IRedisClient>())
+            {
+                PeerResponse = redis.As<PeerResponse>().GetAll().FirstOrDefault(x => x.State == "BlockGenerate");
+                ChainResponse = redis.As<ChainResponse>().GetAll().OrderByDescending(x => x.Timestamp).FirstOrDefault();
+            }
         }
     }
 }
