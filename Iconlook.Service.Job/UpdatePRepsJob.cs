2a823ef8713a978e8bc669c07ad8fea84aaca649
Tom Nuen <tomnuen@iconviet.io>
2019-12-24 15:06:37 +0700

    changed

--- Iconlook.Service.Job/UpdatePRepsJob.cs
+++ Iconlook.Service.Job/UpdatePRepsJob.cs
@@ -20,9 +20,10 @@ namespace Iconlook.Service.Job
     {
         public override async Task RunAsync()
         {
-            using (var time = new Rolex())
+            using (var r1 = new Rolex())
             using (var db = Db.Instance())
             using (var redis = Redis.Instance())
+            using (var json = new JsonHttpClient())
             {
                 var prep_objs = new List<PRep>();
                 var icon = new IconServiceClient();
@@ -30,65 +31,70 @@ namespace Iconlook.Service.Job
                 var prep_info = await icon.GetPRepInfo();
                 await Task.WhenAll(prep_rpcs.Select(prep => Task.Run(async () =>
                 {
-                    string logo_url = null;
-                    var ranking = prep_rpcs.IndexOf(prep) + 1;
-                    prep = await icon.GetPRep(prep.GetAddress());
-                    try
+                    using (var r2 = new Rolex())
                     {
-                        var details = prep.GetDetails();
-                        if (details.HasValue())
+                        string logo_url = null;
+                        var ranking = prep_rpcs.IndexOf(prep) + 1;
+                        prep = await icon.GetPRep(prep.GetAddress());
+                        try
                         {
-                            var cancelation = new CancellationTokenSource();
-                            cancelation.CancelAfter(60000);
-                            var json = new JsonHttpClient();
-                            var response = await json.GetAsync<string>(details, cancelation.Token);
-                            if (response.HasValue())
+                            var details = prep.GetDetails();
+                            if (details.HasValue())
                             {
-                                var @object = DynamicJson.Deserialize(response);
-                                logo_url = @object?.representative?.logo?.logo_256;
+                                var cancelation = new CancellationTokenSource();
+                                cancelation.CancelAfter(60000);
+                                var response = await json.GetAsync<string>(details, cancelation.Token);
+                                if (response.HasValue())
+                                {
+                                    var @object = DynamicJson.Deserialize(response);
+                                    logo_url = @object?.representative?.logo?.logo_256;
+                                    // Log.Information("{Name} latest information loaded in {Elapsed}ms", prep.GetName(), r2.Elapsed.TotalMilliseconds);
+                                }
                             }
                         }
+                        catch
+                        {
+                            Log.Warning("{Name} information failed to load in {Elapsed}ms", prep.GetName(), r2.Elapsed.TotalMilliseconds);
+                        }
+                        prep_objs.Add(new PRep
+                        {
+                            Ranking = ranking,
+                            Name = prep.GetName(),
+                            City = prep.GetCity(),
+                            Joined = DateTime.UtcNow,
+                            State = PRepState.Enabled,
+                            LastSeen = DateTime.UtcNow,
+                            Country = prep.GetCountry(),
+                            Size = new Random().Next(1, 10),
+                            Id = prep.GetAddress().ToString(),
+                            P2PEndpoint = prep.GetP2PEndpoint(),
+                            Score = new Random().Next(-100, 100),
+                            Voters = new Random().Next(100, 1000),
+                            Direction = new Random().NextDouble() >= 0.5,
+                            Balance = new Random().Next(100000, 10000000),
+                            ProducedBlocks = (long) prep.GetTotalBlocks(),
+                            Votes = (long) prep.GetDelegated().ToIcxFromLoop(),
+                            Testnet = new[] { true, false }[new Random().Next(0, 1)],
+                            MissedBlocks = (long) (prep.GetTotalBlocks() - prep.GetValidatedBlocks()),
+                            Entity = new[] { "Company", "Group", "Individual" }[new Random().Next(0, 3)],
+                            Identity = new[] { "Verified", "Unknown", "Anonymous" }[new Random().Next(0, 3)],
+                            Regions = new[] { "Asia", "Europe", "US", "Australia" }[new Random().Next(0, 4)],
+                            Goals = new[] { "Development", "Awareness", "Expansion" }[new Random().Next(0, 3)],
+                            Hosting = new[] { "Azure", "Amazon", "Google", "Bare Metal" }[new Random().Next(0, 4)],
+                            DelegatedPercentage = (double) (prep.GetDelegated().ToDecimal() / prep_info.GetTotalDelegated().ToDecimal()),
+                            ProductivityPercentage = prep.GetValidatedBlocks() > 0 ? (double) (prep.GetValidatedBlocks().ToDecimal() / prep.GetTotalBlocks().ToDecimal()) : 0
+                        }.ThenDo(x =>
+                        {
+                            if (logo_url != null) x.LogoUrl = logo_url;
+                        }));
                     }
-                    catch
-                    {
-                    }
-                    prep_objs.Add(new PRep
-                    {
-                        Ranking = ranking,
-                        LogoUrl = logo_url,
-                        Name = prep.GetName(),
-                        City = prep.GetCity(),
-                        Joined = DateTime.UtcNow,
-                        State = PRepState.Enabled,
-                        LastSeen = DateTime.UtcNow,
-                        Country = prep.GetCountry(),
-                        Size = new Random().Next(1, 10),
-                        Id = prep.GetAddress().ToString(),
-                        P2PEndpoint = prep.GetP2PEndpoint(),
-                        Score = new Random().Next(-100, 100),
-                        Voters = new Random().Next(100, 1000),
-                        Direction = new Random().NextDouble() >= 0.5,
-                        Balance = new Random().Next(100000, 10000000),
-                        ProducedBlocks = (long) prep.GetTotalBlocks(),
-                        Votes = (long) prep.GetDelegated().ToIcxFromLoop(),
-                        Testnet = new[] { true, false }[new Random().Next(0, 1)],
-                        MissedBlocks = (long) (prep.GetTotalBlocks() - prep.GetValidatedBlocks()),
-                        Entity = new[] { "Company", "Group", "Individual" }[new Random().Next(0, 3)],
-                        Identity = new[] { "Verified", "Unknown", "Anonymous" }[new Random().Next(0, 3)],
-                        Regions = new[] { "Asia", "Europe", "US", "Australia" }[new Random().Next(0, 4)],
-                        Goals = new[] { "Development", "Awareness", "Expansion" }[new Random().Next(0, 3)],
-                        Hosting = new[] { "Azure", "Amazon", "Google", "Bare Metal" }[new Random().Next(0, 4)],
-                        DelegatedPercentage = (double) (prep.GetDelegated().ToDecimal() / prep_info.GetTotalDelegated().ToDecimal()),
-                        ProductivityPercentage = prep.GetValidatedBlocks() > 0 ? (double) (prep.GetValidatedBlocks().ToDecimal() / prep.GetTotalBlocks().ToDecimal()) : 0
-                    });
                 })));
                 await db.SaveAllAsync(prep_objs.ToList());
                 redis.StoreAll(prep_objs.ConvertAll(x => x.ToResponse()));
                 Log.Information("**************************************************");
-                Log.Information("{PReps} P-Reps latest information stored in {Elapsed}ms", prep_objs.Count, time.Elapsed.TotalMilliseconds);
+                Log.Information("{PReps} P-Reps latest information stored in {Elapsed}ms", prep_objs.Count, r1.Elapsed.TotalMilliseconds);
                 Log.Information("**************************************************");
             }
-
         }
     }
 }
