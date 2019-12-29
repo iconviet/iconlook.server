ccaae08c6b018abdcb886c23a507eb1f23c37b43
Tom Nuen <tomnuen@iconviet.io>
2019-12-29 22:07:25 +0700

    c

--- Iconlook.Service.Web/Pages/AddressList.razor.cs
+++ Iconlook.Service.Web/Pages/AddressList.razor.cs
@@ -1,14 +1,36 @@
 ﻿using Iconlook.Object;
 using Microsoft.AspNetCore.Components;
+using Syncfusion.EJ2.Blazor.Navigations;
 
 namespace Iconlook.Service.Web.Pages
 {
     public partial class AddressList
     {
+        protected TabHeader Unstaking;
         protected PeerResponse PeerResponse;
         protected ChainResponse ChainResponse;
-        
+        protected TabAnimationSettings Animation;
+
+        [Parameter]
+        public int Size { get; set; }
+
+        public bool Page { get; set; }
+
+        public object Sizes { get; set; }
+
         [Parameter]
         public string State { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Size = 200;
+            Sizes = new[] { 22, 50, 100 };
+            Unstaking = new TabHeader { Text = "UNSTAKING" };
+            Animation = new TabAnimationSettings
+            {
+                Next = new TabAnimationNext { Duration = 0 },
+                Previous = new TabAnimationPrevious { Duration = 0 }
+            };
+        }
     }
 }
---
+++ Iconlook.Service.Web/Pages/AddressListUnstakingGrid.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Iconlook.Service.Web.Pages
+{
+    public partial class AddressListUnstakingGrid
+    {
+        [Parameter]
+        public int PageSize { get; set; }
+
+        [Parameter]
+        public bool CanPage { get; set; }
+
+        [Parameter]
+        public object PageSizes { get; set; }
+    }
+}
