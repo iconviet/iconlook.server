eac9a3056798b6fa5d788c2c473918bb5cd771b8
Tom Nuen <tomnuen@iconviet.io>
2020-01-15 18:49:12 +0700

    changed

--- Iconlook.Service.Web/DecimalExtensions.cs
+++ Iconlook.Service.Web/DecimalExtensions.cs
@@ -5,19 +5,23 @@ namespace Iconlook.Service.Web
 {
     public static class DecimalExtensions
     {
-        public static MarkupString ToMarkupString(this decimal instance)
+        public static MarkupString ToIcxString(this decimal instance)
         {
             var text = instance.ToString("N4");
             var part = text.Split('.');
             if (part.Length == 2)
             {
                 var zero = "number-part";
-                if (int.Parse(part[0], NumberStyles.Number) == 0 && int.Parse(part[1], NumberStyles.Number) == 0)
+                if (int.Parse(part[0], NumberStyles.Number) == 0 &&
+                    int.Parse(part[1], NumberStyles.Number) == 0)
                 {
                     zero = "fraction-part";
                 }
-                text = $"<span class=\"{zero}\">{part[0]}</span>" +
-                       $"<span class=\"fraction-part\">.{part[1]}</span>";
+                text = $"<span class=\"{zero}\">{part[0]}</span>";
+                if (part[1] != "0000")
+                {
+                    text += $"<span class=\"fraction-part\">.{part[1]}</span>";
+                }
             }
             return new MarkupString(text);
         }
