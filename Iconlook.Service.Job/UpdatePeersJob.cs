a9f6420fc769fd277c5faddc001ead31e30f5ecb
Tom Nuen <tomnuen@iconviet.io>
2020-02-26 03:24:14 +0700

    changed

--- Iconlook.Service.Job/UpdatePeersJob.cs
+++ Iconlook.Service.Job/UpdatePeersJob.cs
@@ -57,10 +57,10 @@ namespace Iconlook.Service.Job
                             {
                                 await Channel.Publish(new PeersUpdatedSignal
                                 {
-                                    Idle = peers.Where(x => x.State == "Vote").ToList(),
-                                    Sync = peers.Where(x => x.State == "BlockSync").ToList(),
-                                    Busy = peers.Where(x => x.State == "BlockGenerate").ToList(),
-                                    Down = peers.Where(x => x.State == "LeaderComplain").ToList()
+                                    Idle = peers.Where(x => x?.State == "Vote").ToList(),
+                                    Sync = peers.Where(x => x?.State == "BlockSync").ToList(),
+                                    Busy = peers.Where(x => x?.State == "BlockGenerate").ToList(),
+                                    Down = peers.Where(x => x?.State == "LeaderComplain").ToList()
                                 }).ConfigureAwait(false);
                                 await Task.Run(() => redis.StoreAll(peers)).ConfigureAwait(false);
                             }
