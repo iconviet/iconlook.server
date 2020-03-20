f934aae9714a9f9cd85ffae78f22054d42350c31
Tom Nuen <tomnuen@iconviet.io>
2020-03-21 01:53:16 +0700

    changed

--- Iconlook.Service.Web/WebHost.cs
+++ Iconlook.Service.Web/WebHost.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
+using Agiper;
 using Agiper.Server;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -9,6 +11,7 @@ namespace Iconlook.Service.Web
     {
         public static Task Main()
         {
+            var cu = typeof(WebHost).Assembly.InformationVersion();
             var configuration = new WebHostConfiguration();
             return StartAsync(configuration, b => b.ConfigureWebHostDefaults(x => x.UseStaticWebAssets().UseStartup(configuration.GetType())));
         }
