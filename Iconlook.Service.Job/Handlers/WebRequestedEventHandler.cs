a764df17d3ddb469c3a526a49f67b95244d116e0
Tom Nuen <tomnuen@iconviet.io>
2020-03-21 00:13:15 +0700

    changed

--- Iconlook.Service.Job/Handlers/WebRequestedEventHandler.cs
+++ Iconlook.Service.Job/Handlers/WebRequestedEventHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
 using Agiper;
 using Agiper.Server;
@@ -22,7 +23,7 @@ namespace Iconlook.Service.Job.Handlers
             var user_agent = Parser.GetDefault().Parse(message.UserAgent);
             var html = $"<b>{message.BodyString}</b> {message.IconString}\n" +
                        $"<pre>Hash-ID: {message.UserHashId.SafeSubstring(0, 4)} (friendly)</pre>\n" +
-                       (message.Country.HasValue() ? $"<pre>Country: {message.Country}</pre>\n" : string.Empty) +
+                       (message.Country.HasValue() ? $"<pre>Country: {new RegionInfo(message.Country).DisplayName}</pre>\n" : string.Empty) +
                        (message.Address.HasValue() ? $"<pre>Address: {message.Address}</pre>\n" : string.Empty) +
                        $"<pre>Request: {message.Url}</pre>\n" +
                        (message.Referer.HasValue() ? $"<pre>Referer: {message.Referer}</pre>\n" : string.Empty) +
