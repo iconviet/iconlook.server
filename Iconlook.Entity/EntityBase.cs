1d2dd515b75b2e829af6f8f0f7959d3c0dd0c5e3
Tom Nuen <tomnuen@agiper.com>
2020-04-28 14:00:43 +0700

    changed

--- Iconlook.Entity/EntityBase.cs
+++ Iconlook.Entity/EntityBase.cs
@@ -1,20 +1,8 @@
 ﻿using Iconviet.Object;
-using ReactiveUI;
-using ServiceStack.DataAnnotations;
-using System;
 
 namespace Iconlook.Entity
 {
     public abstract class EntityBase<T> : ObjectBase<T>, IEntity
     {
-        
-        [Ignore]
-        public new IObservable<Exception> ThrownExceptions { get; set; }
-
-        [Ignore]
-        public new IObservable<IReactivePropertyChangedEventArgs<IReactiveObject>> Changed { get; set; }
-
-        [Ignore]
-        public new IObservable<IReactivePropertyChangedEventArgs<IReactiveObject>> Changing { get; set; }
     }
 }
