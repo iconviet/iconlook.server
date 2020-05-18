adb6d760f74649227f0a86ca1d66b80715bcf569
Tom Nuen <tomnuen@iconviet.io>
2020-05-18 14:52:20 +0700

    fixed header & cookie middleware

--- Iconlook.Service.Web/CookieProcessorMiddleware.cs
+++ Iconlook.Service.Web/CookieProcessorMiddleware.cs
@@ -23,37 +23,19 @@ namespace Iconlook.Service.Web
 
         public async Task InvokeAsync(HttpContext http)
         {
-            var new_user_hash_id = string.Empty;
             var url = http.Request.GetDisplayUrl();
             var referer = http.Request.Headers["Referer"].ToString();
             var country = http.Request.Headers["CF-IPCountry"].ToString();
+            var user_hash_id = http.Request.Cookies[Cookies.USER_HASH_ID];
             var user_agent = http.Request.Headers["User-Agent"].ToString();
             var address = http.Request.Headers["CF-Connecting-IP"].ToString();
-            var old_user_hash_id = http.Request.Cookies[Cookies.USER_HASH_ID];
             http.Response.OnStarting(x =>
             {
                 var state = (HttpContext) x;
                 if (!state.Request.Path.StartsWithSegments("/_blazor"))
                 {
-                    if (old_user_hash_id.IsNullOrEmpty())
-                    {
-                        new_user_hash_id = Generate.HashId(16).ToUpper();
-                        state.Response.Cookies.Append(
-                            Cookies.USER_HASH_ID, new_user_hash_id, new CookieOptions
-                            {
-                                Expires = DateTime.UtcNow.AddYears(1)
-                            });
-                    }
-                }
-                return Task.CompletedTask;
-            }, http);
-            http.Response.OnCompleted(x =>
-            {
-                var state = (HttpContext) x;
-                if (!state.Request.Path.StartsWithSegments("/_blazor"))
-                {
                     var endpoint = _application.ApplicationServices.TryResolve<IMessageSession>();
-                    if (old_user_hash_id.HasValue())
+                    if (user_hash_id.HasValue())
                     {
                         endpoint.Publish(new WebRequestedEvent
                         {
@@ -63,21 +45,27 @@ namespace Iconlook.Service.Web
                             Country = country,
                             IconString = "🔸",
                             UserAgent = user_agent,
-                            UserHashId = old_user_hash_id,
+                            UserHashId = user_hash_id,
                             BodyString = "OLD USER REVISITED"
                         }).ConfigureAwait(false);
                     }
-                    if (new_user_hash_id.HasValue())
+                    else
                     {
+                        user_hash_id = Generate.HashId(16).ToUpper();
                         endpoint.Publish(new WebRequestedEvent
                         {
                             Url = url,
                             Referer = referer,
                             IconString = "💠",
                             UserAgent = user_agent,
-                            UserHashId = new_user_hash_id,
+                            UserHashId = user_hash_id,
                             BodyString = "NEW USER DETECTED"
                         }).ConfigureAwait(false);
+                        state.Response.Cookies.Append(
+                            Cookies.USER_HASH_ID, user_hash_id, new CookieOptions
+                            {
+                                Expires = DateTime.UtcNow.AddYears(1)
+                            });
                     }
                 }
                 return Task.CompletedTask;
